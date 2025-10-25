7bfd96428b565a9db8a6bfefd7ee456204071416
Andrii Yermolenko <vakt159@gmail.com>
2025-10-25 18:47:07 +0300

    Completed py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result_list = []
+
+    for human in people:
+        result_list.append(Person(name=human["name"], age=human["age"]))
+
+    for human in people:
+        if human.get("wife", 0):
+            Person.people[human["wife"]].husband = Person.people[human["name"]]
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+    return result_list
