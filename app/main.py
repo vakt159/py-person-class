b6144f881ab8ceebddad8127cf595e1fee482cad
Andrii Yermolenko <vakt159@gmail.com>
2025-10-26 17:54:12 +0200

    Changed method to access dict keys to dict.get() and people dict initialisation in Person __init__

--- app/main.py
+++ app/main.py
@@ -5,16 +5,17 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     result_list = [Person(name=human["name"], age=human["age"])
                    for human in people]
 
-    for i in range(len(people)):
-        if "wife" in people[i] and people[i]["wife"] is not None:
-            result_list[i].wife = Person.people[people[i]["wife"]]
-        if "husband" in people[i] and people[i]["husband"] is not None:
-            result_list[i].husband = Person.people[people[i]["husband"]]
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        elif person.get("husband"):
+            Person.people[person["name"]].husband = (
+                Person.people)[person["husband"]]
     return result_list
